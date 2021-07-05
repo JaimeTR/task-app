f9f3e485ec67e5c47a654e156eacddffb43fb5b5
Josué Ayala <ayalavalencia11@gmail.com>
2021-07-05 13:39:05 -0600

    😋 baseURL in axios prop

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,9 @@ export default {
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    baseURL: process.env.BASE_URL
+  },
 
   publicRuntimeConfig: {
     axios: {
