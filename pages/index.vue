aac6326b5b081ca13061992bea591b8efe9b3809
Josué Ayala <ayalavalencia11@gmail.com>
2021-07-05 13:29:30 -0600

    👾 Global axios config

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex items-center justify-center h-screen">
+    <button class="bg-red-200 hover:bg-red-300 p-4" @click="createRequest()">Request</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  layout: 'default',
+  methods: {
+    async createRequest(){
+      try {
+        await this.$axios.$get('task');
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  }
+}
+</script>
